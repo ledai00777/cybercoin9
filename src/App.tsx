0219c0dd59d65bfd9e3a7c31d4349357903f15f3
ledai00777 <ledai00777@gmail.com>
2026-07-27 02:46:39 +0700

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import CrewDatabase from './components/CrewDatabase';
 import Tokenomics from './components/Tokenomics';
 import CyberpsychoMeter from './components/CyberpsychoMeter';
 import Roadmap from './components/Roadmap';
+import Quotes from './components/Quotes';
 import HowToBuy from './components/HowToBuy';
 import FAQ from './components/FAQ';
 import Footer from './components/Footer';
@@ -37,6 +38,7 @@ export default function App() {
         <Tokenomics />
         <CyberpsychoMeter />
         <Roadmap />
+        <Quotes />
         <HowToBuy />
         <FAQ />
       </main>
